9bc6243670fd9d93cd9332d3b106863ca451fe06
debugger2017 <kartik.p.satoskar@gmail.com>
2017-01-31 22:54:53 +0530

    Added search action

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,15 +1,20 @@
 class InvitationsController < ApplicationController
   def new
+  	@invitation = Invitation.new
   end
 
   def show
   end
 
-  def create
+  def search
   	name = (params[:invite][:name]).downcase
   	result = User.select(:id).where("lower(name) like ?" , "%#{name}%")
  	flash[:invite] = result
   	redirect_to new_invitation_path
+  end
+
+  def create
+  	user = User.find_by(id: params[:invite][:id])
   end
 
 end
