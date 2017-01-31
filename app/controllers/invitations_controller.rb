ad0455638c24d28389d242d37fab37a05f959add
debugger2017 <kartik.p.satoskar@gmail.com>
2017-01-31 20:02:34 +0530

    Make somechanges

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -4,4 +4,12 @@ class InvitationsController < ApplicationController
 
   def show
   end
+
+  def create
+  	name = (params[:invite][:name]).downacase
+  	result = User.select(:id).where("lower(name) like ?" , "#{name}")
+ 	session[:invite] = result
+  	redirect_to new_invitation_path
+  end
+
 end
