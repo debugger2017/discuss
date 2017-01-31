631505b14b5b9517fd930a987612616c0c188ac1
debugger2017 <kartik.p.satoskar@gmail.com>
2017-01-31 17:14:47 +0530

    Fixed bug in controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,17 +4,25 @@ class GroupsController < ApplicationController
 	end
 
 	def new
+		@user = current_user
 		@group = Group.new
 	end
 
 	def create
-
+		@user = current_user
+		@group = Group.new(group_params)
+  		if @group.save
+      	  flash[:success] = "Created group!!"
+      	  redirect_to @group
+  		else
+  		  render 'new'
+  	    end
 	end
 
 
 
 	private 
-	
+
 	def group_params
 		params.require(:group).permit(:name)
 	end
