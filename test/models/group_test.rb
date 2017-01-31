5aca1192d980b8dc52b5a21fea4fcd6be0001fb1
debugger2017 <kartik.p.satoskar@gmail.com>
2017-01-31 16:50:03 +0530

    Added tests for length and presence

--- test/models/group_test.rb
+++ test/models/group_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class GroupTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	def setup
+		@group = groups(:idyllic)
+	end
+
+	test "should be valid" do 
+    	assert @group.valid?
+  	end
+
+	test "name should be present" do
+		@group.name = " "
+		assert_not @group.valid?
+	end
 end
