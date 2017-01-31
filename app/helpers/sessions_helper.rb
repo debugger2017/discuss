cdb37ce128d6a2638514b9593b46a8b7ac5c0f79
debugger2017 <kartik.p.satoskar@gmail.com>
2017-01-31 13:28:44 +0530

    Added helper methods

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,7 +4,11 @@ module SessionsHelper
 	end
 
 	def current_user
-		User.find_by(id: session[:user_id])
+		@current_user ||= User.find_by(id: session[:user_id])
+	end
+
+	def logged_in?
+		!current_user.nil?
 	end
 
 
