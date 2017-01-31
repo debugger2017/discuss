ae1576839c473c3c13b0b56a2784f651e49efb40
debugger2017 <kartik.p.satoskar@gmail.com>
2017-01-31 18:50:17 +0530

    Added attributes

--- db/migrate/20170131131428_create_invitations.rb
+++ db/migrate/20170131131428_create_invitations.rb
@@ -1,7 +1,11 @@
 class CreateInvitations < ActiveRecord::Migration[5.0]
   def change
     create_table :invitations do |t|
-
+      t.integer :sender_id
+      t.integer :receiver_id
+      t.integer :group_id
+      t.boolean :is_read
+      t.boolean :is_accepted
       t.timestamps
     end
   end
