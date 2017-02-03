455c713b32e96b8350b3a1ffbe3f0012dfe04f4d
debugger2017 <kartik.p.satoskar@gmail.com>
2017-02-03 10:33:41 +0530

    Partially done with paperclip

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,11 +10,13 @@ class PostsController < ApplicationController
   end
 
   def create
+    tmp = params[:post][:avatar]
+    puts tmp
   	user = current_user.id
   	group = params[:post][:id].to_i
   	membership = Membership.select(:id).where(user_id: user, group_id: group).first
   	post_content = params[:post][:content]
-  	new_post = Post.new(content: post_content, membership_id: membership['id'])
+  	new_post = Post.new(content: post_content, membership_id: membership['id'],avatar_file_name:tmp.original_filename)
   	new_post.save
   	group_object = Group.find_by(id: group)
   	redirect_to group_path(group_object)
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,6 @@ class Post < ApplicationRecord
 	#belongs_to :group 
 	has_many :comments , dependent: :destroy
 
-	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "app/assets/images/missing.png", url: '/public/:filename'
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
